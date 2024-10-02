b3f692c62ea9af1ce38c734028194b6a01d22469
aavila <aavila@casa.bo>
2024-10-02 09:35:04 -0400

    Correccion archivo clean

--- ProySanitarioOLTP/ProySanitarioOLTP/Scripts/Clean.sql
+++ ProySanitarioOLTP/ProySanitarioOLTP/Scripts/Clean.sql
@@ -1,20 +1,20 @@
 ﻿delete from HistoriaClinica
-DBCC CHECKIDENT ('HistoriaClinica', RESEED,0)
+DBCC CHECKIDENT ('HistoriaClinica', RESEED)
 
 delete from HospitalServicioMedico
-DBCC CHECKIDENT ('HospitalServicioMedico', RESEED,0)
+DBCC CHECKIDENT ('HospitalServicioMedico', RESEED)
 
 delete from Hospital
-DBCC CHECKIDENT ('Hospital', RESEED,0)
+DBCC CHECKIDENT ('Hospital', RESEED)
 
 delete from Medico
-DBCC CHECKIDENT ('Medico', RESEED,0)
+DBCC CHECKIDENT ('Medico', RESEED)
 
 delete from Paciente
-DBCC CHECKIDENT ('Paciente', RESEED,0)
+DBCC CHECKIDENT ('Paciente', RESEED)
 
 delete from Servicio
-DBCC CHECKIDENT ('Servicio', RESEED,0)
+DBCC CHECKIDENT ('Servicio', RESEED)
 
 
 
